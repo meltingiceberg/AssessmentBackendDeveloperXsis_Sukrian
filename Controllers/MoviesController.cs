37d674ec18e604df7330a9513159c07061675e46
Sukrian Syahnel <rian.syahnel@gmail.com>
2023-10-24 00:48:17 +0700

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssessmentBackendDeveloperXsis_Sukrian.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ILogger<MoviesController> _logger;
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
